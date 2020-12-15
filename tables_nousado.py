deb533565bd05b94b159411ecfaef49ace0c837f
lsoriai <leyryky@hotmail.com>
2020-12-15 11:41:42 +0100

    mar-15-12-2020_4

--- tables_nousado.py
+++
@@ -1,8 +0,0 @@
-import django_tables2 as tables
-from django_tables2.utils import A
-from .models import Person
-
-class PersonTable(tables.Table):
-    class Meta:
-        model = Person
-        template_name = 'django_tables2/bootstrap.html'
